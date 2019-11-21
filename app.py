0b8020420470d86ea2942c689131a49d018bfa8f
stunt296 <38454056+stunt296@users.noreply.github.com>
2019-11-21 17:52:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/base.html')
+def base():
+    return render_template("base.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
